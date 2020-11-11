f3302eb21551e85030c0966a9d51c61dbddf5532
Veronica <terrence_bogard@hotmail.com>
2020-11-12 00:20:40 +0100

    Ejercicio 2 de la unidad 2 resuelto

---
+++ src/unidad2/Conversor.java
@@ -0,0 +1,16 @@
+package unidad2;
+
+public class Conversor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double euro = 3.6; 
+		double cambio = 1.18;
+		double dolar = (euro*cambio);
+		double entero = Math.floor(dolar);
+		double decimal = Math.ceil((dolar-entero) * 100)/100;
+		System.out.println(euro + " euros equivalen a " + (entero+decimal) + " dólares ");
+
+	} // fin de main
+
+} // fin de la clase Conversor
